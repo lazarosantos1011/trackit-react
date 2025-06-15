c513afe5e6aa5c693d26925a2355bf8cd0c36ab7
Lázaro Rocha <lazaro.rocha1011@gmail.com>
2025-06-15 20:26:09 -0300

    refactor: update .gitignore for Python cache files and optimize image import in Header component

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -1,7 +1,9 @@
+import logo from '../assets/images/TrackIT_logo.png';
+
 const Header = ({ onLogout, showLogout = false }) => (
   <header id="header">
     <div className="header__logo-container">
-      <img src="/assets/images/TrackIT_logo.png" alt="Logo do TrackIT" id="logo" />
+      <img src={logo} alt="Logo do TrackIT" id="logo" />
       <h1>TrackIT</h1>
     </div>
     {showLogout && (
